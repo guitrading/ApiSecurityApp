55f07272f7098f95647dd0f41dd723a4892b9e97
Guilherme Reis <guitrading@outlook.com>
2023-06-10 05:26:35 -0400

    update controllers

--- ApiSecurity/Controllers/AuthenticationController.cs
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -1,62 +1,96 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ApiSecurity.Controllers;
-
 
 [Route("api/[controller]")]
 [ApiController]
 public class AuthenticationController : ControllerBase
 {
+    private readonly IConfiguration _config;
 
-    public record AuthenticationData(string? UserName, string?Password); // equivalent of a creating a classes
-    // with two properties and setting up a constructor that takes two parameters. Readonly object.
-    public record UserData(int UserId, string UserName); 
-
-    [HttpPost("token")]  // route becomes api/authentication/token
-    public ActionResult<string> Authenticate([FromBody] AuthenticationData data) // method to authenticate user
+    public AuthenticationController(IConfiguration config)
     {
-        return Ok("Authenticated");
+        _config = config;
     }
 
-    private UserData ValidateCredentials(AuthenticationData data)
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int UserId, string UserName, string Title, string EmployeeId);
+
+    // api/Authentication/token
+    [HttpPost("token")]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
     {
-        // THIS IS NOT PRODUCTION CODE
-        // DO NOT STORE PASSWORDS IN PLAIN TEXT
-        // This is simulating a database lookup
+        var user = ValidateCredentials(data);
 
-        if (CompareValues(data.UserName, "admin") && CompareValues(data.Password, "password"))
+        if (user is null)
         {
-            return new UserData(1, data.UserName!);
-        }
-        else
-        {
-            throw new Exception("Invalid Credentials");
+            return Unauthorized();
         }
 
-        if (CompareValues(data.UserName, "sstorm") && CompareValues(data.Password, "password"))
-        {
-            return new UserData(1, data.UserName!);
-        }
-        else
-        {
-            throw new Exception("Invalid Credentials");
-        }
+        var token = GenerateToken(user);
 
+        return Ok(token);
     }
 
-    private bool CompareValues(string? actual, string expected) 
-    { 
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(
+            Encoding.ASCII.GetBytes(
+                _config.GetValue<string>("Authentication:SecretKey")));
+
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.UserId.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+        claims.Add(new("title", user.Title));
+        claims.Add(new("employeeId", user.EmployeeId));
+
+
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow, // When this token becomes valid
+            DateTime.UtcNow.AddMinutes(1), // When the token will expire
+            signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        // THIS IS NOT PRODUCTION CODE - THIS IS ONLY A DEMO - DO NOT USE IN REAL LIFE
+        if (CompareValues(data.UserName, "tcorey") &&
+            CompareValues(data.Password, "Test123"))
+        {
+            return new UserData(1, data.UserName!, "Business Owner", "E001");
+        }
+
+        if (CompareValues(data.UserName, "sstorm") &&
+            CompareValues(data.Password, "Test123"))
+        {
+            return new UserData(2, data.UserName!, "Head of Security", "E005");
+        }
+
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
         if (actual is not null)
         {
             if (actual.Equals(expected))
             {
                 return true;
             }
-
-            
         }
+
         return false;
     }
-
 }
--- ApiSecurity/Program.cs
+++ ApiSecurity/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication: Issuer)"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication: Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
 
 var app = builder.Build();
 
@@ -17,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
